30e5a8904e4c24dea8c5b1b5843f8f2c36e5f29d
363311953@qq.com <363311953@qq.com>
2020-06-24 18:06:55 +0800

    中奖列表

--- pages/deliveryDetails/index.js
+++ pages/deliveryDetails/index.js
@@ -1,14 +1,28 @@
 // pages/delivery/index.js
+import pubFun from '../../utils/pubFun'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    luckData:[
-      {types:'进行中',title:'哇哦999999999999999元现金大红包',number:'8989',time:'2020/08/09-2020/09/08',timeInterval:'08:09–18:19',tys:'0'},
-      {types:'待开始',title:'哇哦9毛红包',number:'8989',time:'2020/08/09-2020/09/08',tys:'1'},
+    prizeData: [{
+        imgs: 'img/3.png',
+        name: '波音747播放的747',
+        readyNum: '8',
+        unNun: '89'
+      },
+      {
+        imgs: 'img/3.png',
+        name: '波音747播放的747',
+        readyNum: '8',
+        unNun: '89'
+      },
     ],
+    openOver: false,
+    currentDate: new Date().getTime() + 86400000,
+    minDate: new Date().getTime(),
+    openTime: pubFun.formatDuring((new Date()).getTime() + 86400000), //开奖时间
   },
 
   /**
@@ -17,7 +31,28 @@ Page({
   onLoad: function (options) {
 
   },
-
+  // 选择时间弹窗
+  openTimeFun() {
+    this.setData({
+      openOver: true
+    })
+  },
+  // 关闭时间选择弹窗
+  onClose() {
+    this.setData({
+      openOver: false
+    })
+  },
+  //确认开奖时间
+  onInput(event) {
+    console.log()
+    let openTime = pubFun.formatDuring(event.detail)
+    this.setData({
+      openTime: openTime,
+      openOver: false
+    })
+    console.log(openTime)
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
